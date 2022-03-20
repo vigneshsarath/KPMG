e3e915fa356c95589575eed00e62b6e6fe309c40
vigneshsarath <38080447+vigneshsarath@users.noreply.github.com>
2022-03-20 20:15:27 +0000

    Update nest.py

--- Challenge3/nest.py
+++ Challenge3/nest.py
@@ -1,7 +1,6 @@
 import json
 class obj:
-    def __init__(self, testName, object, key):
-        self.testName = testName
+    def __init__(self, object, key):
         self.object = object
         self.key = key
     def json_value(self):
@@ -9,4 +8,4 @@ class obj:
         dict = json.loads(self.object)
         for self.key in keys:
             dict = dict[self.key]
-        return dict 
+        return dict
