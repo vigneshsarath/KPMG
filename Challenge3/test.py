68c34f7becb0b26720b00e5d40ab0ca22b5f1637
vigneshsarath <38080447+vigneshsarath@users.noreply.github.com>
2022-03-20 20:16:28 +0000

    Update test.py

--- Challenge3/test.py
+++ Challenge3/test.py
@@ -2,8 +2,8 @@ import sys
 sys.path.append(".")
 from nest import obj
 def test_json1():
-    test1 = obj("Test 1", '{"a":{"b":{"c":"d"}}}', 'a/b/c')
+    test1 = obj('{"a":{"b":{"c":"d"}}}', 'a/b/c')
     assert test1.json_value() == 'd', 'Should be d'
 def test_json2():
-    test2 = obj("Test 2", '{"x":{"y":{"z":"a"}}}', 'x/y/z')
+    test2 = obj('{"x":{"y":{"z":"a"}}}', 'x/y/z')
     assert test2.json_value() == 'a', 'Should be a'
